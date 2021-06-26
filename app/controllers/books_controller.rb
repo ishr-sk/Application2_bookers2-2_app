96c703a8054fab12c263c66af909898ef27867f4
ishr-sk <kmrttnr1208@gmail.com>
2021-06-26 13:27:20 +0000

    [更新]booksのviews

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,27 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book)
   end
 
   def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
   
   def destroy
