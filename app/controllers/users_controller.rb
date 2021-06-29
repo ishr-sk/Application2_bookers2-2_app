33e1aab4cd59b0d8ecd4c036bbaf287305f79428
ishr-sk <kmrttnr1208@gmail.com>
2021-06-29 15:54:30 +0000

    [更新]user詳細修正(編集部分)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
   end
 
 # 自分の投稿bookを表示
@@ -10,7 +13,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+  # ユーザid探す
     @user = User.find(params[:id])
+  # もしユーザがログインユーザだったらそのまま編集に進む
+    if @user == current_user
+      render :edit
+  # その他のユーザは自分のユーザ詳細が画面に飛ぶ
+    else
+      redirect_to user_path(current_user)
+    end
   end
   
   def update
