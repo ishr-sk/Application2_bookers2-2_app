f6b01e2d3fbd4afb3e9dbd783aba048242cf6d86
ishr-sk <kmrttnr1208@gmail.com>
2021-06-30 04:35:49 +0000

    [更新]booksエラー文表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,18 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
+    # インスタント変数じゃないとエラー分が表示されなかった
+    @book = Book.new(book_params)
     #　投稿したidはログインしてるユーザidである(大事)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book)
+    @book.user_id = current_user.id
+    # バリデーションの結果によりリダイレクト先変わる
+    if @book.save
+      redirect_to book_path(book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -26,12 +33,21 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user 
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    # ローカル変数だとデータが入らなかったからインスタント変数にする
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+      redirect_to book_path(book)
+    else
+      render :edit
+    end
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   attachment :profile_image
+  
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :body, length: { maximum: 50 }
 end
