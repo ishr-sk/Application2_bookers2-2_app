4af192c7973d4e621b0583182d9f8737adc2ffa8
ishr-sk <kmrttnr1208@gmail.com>
2021-06-30 03:22:17 +0000

    [更新]about・ログインorアウトヘッダー表示変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  # ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+  
   protected
   
   def configure_permitted_parameters
