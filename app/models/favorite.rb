f1620101421dd3ac8a98852319213be5d56e064b
ishr-sk <kmrttnr1208@gmail.com>
2021-07-07 09:03:31 +0000

    [追加]favoriteモデルにバリデーションを追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,7 @@ class Favorite < ApplicationRecord
     # アソシエーションの定義
     belongs_to :user
     belongs_to :book
+    # １人が１つの投稿に対して、１つしかいいねがつけられないようにする
     validates_uniqueness_of :book_id, scope: :user_id
 end
 
